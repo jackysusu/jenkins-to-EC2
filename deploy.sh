ab06e4a3147170bbfd12b339c7cc920a7eee11a4
jacky <jackysusu@gmail.com>
2022-08-29 23:20:12 +0800

    3

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+aws configure set default.region us-east-1; aws configure set aws_access_key_id 'AKIARD4PRF6RAAOJYLN6' ; aws configure set aws_secret_access_key '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' | sudo sh
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 077073559458.dkr.ecr.ap-northeast-1.amazonaws.com/nginx
 docker pull 077073559458.dkr.ecr.ap-northeast-1.amazonaws.com/nginx:latest
 #aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}
