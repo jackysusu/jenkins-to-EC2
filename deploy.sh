d17c11dd75cfcb6ff691d94b1e6b57987f2fa95f
jacky <jackysusu@gmail.com>
2022-08-29 22:58:54 +0800

    3

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 077073559458.dkr.ecr.ap-northeast-1.amazonaws.com/repo_test
+#docker pull 077073559458.dkr.ecr.ap-northeast-1.amazonaws.com/nginx:latest
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}
+docker pull ${REPOSITORY_URI}:latest
